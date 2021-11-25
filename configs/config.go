0ab2f2daa123718599764a42af2c50e59bdffc60
StarYuhen <3446623843@qq.com>
2021-11-25 10:05:56 +0800

    新增迁移后的小程序源码和jwt，跨域网关，mysql连接池

--- configs/config.go
+++ configs/config.go
@@ -1,6 +1,24 @@
 package configs
 
-import _ "embed"
+import (
+	_ "embed"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
 
 //go:embed config.yaml
 var configFile []byte
+
+var Config = WriteConfig()
+
+// WriteConfig 读取配置文件内容写入结构体
+func WriteConfig() config {
+	var con config
+	// 将文件内容映射进入结构体
+	if err := yaml.UnmarshalStrict(configFile, &con); err != nil {
+		logrus.Error("读取文件映射结构体出错")
+		panic(err)
+	}
+	logrus.Info("读取到的yaml配置文件的值----->", con)
+	return con
+}
--- configs/struct.go
+++ configs/struct.go
@@ -2,8 +2,19 @@ package configs
 
 // 定义框架结构体
 type config struct {
+	AppVersion  int   `yaml:"AppVersion" json:"AppVersion"`
+	MysqlConfig mysql `yaml:"MYSQLConfig" json:"MYSQLConfig"`
 }
 
+// mysql配置环境
 type mysql struct {
-	User string `json:"user" yaml:"user"`
+	UserConfig  configMysql `json:"user" yaml:"UserConfig"`
+	UserDBTable []string    `yaml:"UserDBTable" json:"UserDBTable"`
+}
+
+// 数据库账号配置文件内容
+type configMysql struct {
+	User         string `yaml:"User"`
+	Password     string `yaml:"Password"`
+	Architecture string `yaml:"Architecture"`
 }
